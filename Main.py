4f03edf99a0df6a0124a85c3f6b49ba4ad3f8715
cchernn <cchernn@gmail.com>
2024-10-05 23:14:05 +0800

    Main to remove local operations

--- Main.py
+++ Main.py
@@ -5,12 +5,6 @@ import Setup
 import json
 import re
 
-from sample_event import event
-# from sample_event3 import event
-# from sample_event4 import event
-# from sample_event5 import event
-# from sample_event6 import event
-
 routes = [
     ("/transactions", "GET", Transactions.list),
     ("/transactions", "POST", Transactions.create),
@@ -27,8 +21,7 @@ def main(event, context):
             result = route_func(params, db)
             return {
                 'statusCode': 200,
-                # 'body': json.dumps(result)
-                'body': result,
+                'body': json.dumps(result)
             }
     return {
         'statusCode': 400,
@@ -39,9 +32,3 @@ def admin(event, context):
     params = Params(event)
     db = Database(params)
     Setup.main(params, db)
-
-response = main(event, None)
-with open("response.json", "w") as fp:
-    json.dump(response, fp)
-
-# admin(event, None)
