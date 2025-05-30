870d9045b9ac6693c55d875cb745beea3c4b64d2
cchernn <cchernn@gmail.com>
2025-05-31 02:55:09 +0800

    update Test file

--- Test.py
+++ Test.py
@@ -3,16 +3,26 @@ import json
 
 if __name__ == "__main__":
     files = [
-        "transactions_list",
-        "groups_list",
-        "locations_list",
-        "attachments_list"
-        # "transactions_create",
+        # "transactions_list",
+        # "transactions_create", # pending group and attachment
         # "transactions_get",
-        # "transactions_edit",
+        # "transactions_edit", # pending group and attachment
         # "transactions_delete",
-        # "groups_create",
+        # "groups_list", 
+        # "groups_create", # pending user
+        # "groups_get",
+        # "groups_edit", # pending user
+        # "groups_delete",
+        # "locations_list",
         # "locations_create",
+        # "locations_get",
+        # "locations_edit",
+        # "locations_delete",
+        # "attachments_list"
+        # "attachments_create"
+        # "attachments_get"
+        # "attachments_edit"
+        # "attachments_delete"
     ]
     for filename in files:
         with open(f"test/params/{filename}.json", "r") as fp:
